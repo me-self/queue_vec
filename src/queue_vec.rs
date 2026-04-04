46b27c50e505592c1296ddea895e2bc871a814f5
sandmuel <samuel@sandmuel.net>
2026-04-03 21:51:33 -0700

    Use relaxed length where possible

--- src/queue_vec.rs
+++ src/queue_vec.rs
@@ -22,10 +22,11 @@ impl<T> QueueVec<T> {
     }
 
     pub fn get(&self, index: usize) -> Option<&T> {
-        // TODO: unnecessary acquire.
         self.vec.get(index).or_else(|| {
             println!("Index is in queue.");
-            self.queue.get(index - self.vec.len())
+            // It's ok to use `reserved_len` here since we aren't reading elements from the
+            // vec itself.
+            self.queue.get(index - self.vec.reserved_len())
         })
     }
 
@@ -33,7 +34,7 @@ impl<T> QueueVec<T> {
         let queue = take(&mut self.queue);
 
         let new_items = queue.into_iter();
-        let total_len = self.vec.len() + new_items.len();
+        let total_len = self.vec.reserved_len() + new_items.len();
         let new_length = total_len.next_power_of_two();
 
         self.vec.realloc(new_length);
